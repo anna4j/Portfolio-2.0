52a2dee962fa0df3d93c99d34e38cae80705d2b7
anna4j <anushkajadhav004@gmail.com>
2020-08-13 17:02:29 +0530

    change bullet pt

--- pages/generic.js
+++ pages/generic.js
@@ -29,10 +29,10 @@ export default () => (
                                 </header>
                                 <h4>Technology Stack</h4>
 
-                                <p> Uses ARIMA Time Series Forecasting to predict district wise Crop Yield in advance, for major produced crops in India.</p>
-                                <p> Remote sensing NDVI data available for various Indian Districts was used for model-training. Attained MSE less than 0.2</p>
-                                <p> Uses Android for User-Interface and Flask for Model API deployment. API hosted on Azure platform.</p>
-                                <p> Uses DialogFlow chatbot by Google Cloud for answering user queries.</p>
+                                <p>{`>`} Uses ARIMA Time Series Forecasting to predict district wise Crop Yield in advance, for major produced crops in India.</p>
+                                <p>{`>`} Remote sensing NDVI data available for various Indian Districts was used for model-training. Attained MSE less than 0.2</p>
+                                <p>{`>`} Uses Android for User-Interface and Flask for Model API deployment. API hosted on Azure platform.</p>
+                                <p>{`>`} Uses DialogFlow chatbot by Google Cloud for answering user queries.</p>
 
 
                                 <ul className="actions">
@@ -52,9 +52,9 @@ export default () => (
                                 </header>
                                 <h4>Impact – To improve current condition of Indian Farmers
                                         </h4>
-                                <p> Impactful for Government officials to prepare a proper procurement plan and manage ground-level logistics duringharvesting season.</p>
+                                <p>{`>`} Impactful for Government officials to prepare a proper procurement plan and manage ground-level logistics duringharvesting season.</p>
 
-                                <p> Can improve Food Security and help prevent Famine in India.</p>
+                                <p>{`>`} Can improve Food Security and help prevent Famine in India.</p>
                                 <ul className="actions">
                                     <li><Link href="https://docs.google.com/presentation/d/1f2-bG_s6g5MnjcBrruXQkoc5zLp4T6DfQV6S4CrUZJM/edit?usp=sharing"><a target="_blank" className="button">Learn More</a></Link></li>
                                 </ul>
